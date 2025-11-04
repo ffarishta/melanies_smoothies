a493be398563611d1e0b83d1ae4a4515f8f8a413
Fatima Farishta <62407982+ffarishta@users.noreply.github.com>
2025-11-04 09:11:38 -0500

    Add pandas import and convert DataFrame to pandas

--- streamlit_app.py
+++ streamlit_app.py
@@ -2,6 +2,7 @@
 import streamlit as st
 from snowflake.snowpark.functions import col, when_matched
 import requests
+import pandas as pd
 
 # Write directly to the app
 st.title(f":cup_with_straw: Customize Your Smoothie :cup_with_straw:")
@@ -19,6 +20,9 @@ cnx = st.connection("snowflake")
 session= cnx.session()
 my_dataframe= session.table("smoothies.public.fruit_options").select(col('FRUIT_NAME'))
 #st.dataframe(data=my_dataframe, use_container_width=True)
+pdf_df=my_dataframe.to_pandas()
+#st.dataframe(pdf_df)
+#st.stop()
 
 ingredients_list = st.multiselect(
     'Choose up to 5 Ingredients',
@@ -31,6 +35,8 @@ if ingredients_list:
 
     for i in ingredients_list:
         ingredients_string += i + " "
+        search_on=pd_df.loc[pd_df['FRUIT_NAME'] == fruit_chosen, 'SEARCH_ON'].iloc[0]
+        st.write('The search value for ', fruit_chosen,' is ', search_on, '.')
         st.subheader(i+'Nutrition Information')
         smoothiefroot_response = requests.get("https://my.smoothiefroot.com/api/fruit/"+i)
         df_df = st.dataframe(data=smoothiefroot_response.json(), use_container_width=True)
