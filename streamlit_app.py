02b358d3bf461c3cccc450ebe6affbf0ced1ab2e
Fatima Farishta <62407982+ffarishta@users.noreply.github.com>
2025-11-04 09:07:35 -0500

    Update dataframe handling in Streamlit app
    Comment out the original dataframe display and convert the Snowflake dataframe to a pandas dataframe before displaying it.

--- streamlit_app.py
+++ streamlit_app.py
@@ -19,7 +19,11 @@ st.write("The name of your smoothie will be: ", name_on_order)
 cnx = st.connection("snowflake")
 session= cnx.session()
 my_dataframe= session.table("smoothies.public.fruit_options").select(col('FRUIT_NAME'),col('SEARCH_ON'))
-st.dataframe(data=my_dataframe, use_container_width=True)
+#st.dataframe(data=my_dataframe, use_container_width=True)
+#st.stop()
+
+pd_df=my_dataframe.to_pandas()
+st.dataframe(pd_df)
 st.stop()
 
 ingredients_list = st.multiselect(
@@ -33,6 +37,8 @@ if ingredients_list:
 
     for i in ingredients_list:
         ingredients_string += i + " "
+        search_on=pd_df.loc[pd_df['FRUIT_NAME'] == fruit_chosen, 'SEARCH_ON'].iloc[0]
+        st.write('The search value for ', fruit_chosen,' is ', search_on, '.')
         st.subheader(i+'Nutrition Information')
         smoothiefroot_response = requests.get("https://my.smoothiefroot.com/api/fruit/"+i)
         df_df = st.dataframe(data=smoothiefroot_response.json(), use_container_width=True)
